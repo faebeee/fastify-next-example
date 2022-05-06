02a3805f8f0c024798145f75ff088502d56861a9
Fabio Gianini <faebeee@gmail.com>
2022-05-06 19:05:20 +0200

    Make setup working

--- src/plugins/next.ts
+++
@@ -1,9 +0,0 @@
-import fp from 'fastify-plugin'
-import next from '@fastify/nextjs';
-
-export default fp(async (fastify, opts) => {
-    fastify.register(next)
-        .after(() => {
-            fastify.next('/home')
-        })
-})
